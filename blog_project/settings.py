76c410df217be5ad25630d997a9d720e062e5041
Peizhi <parisa1206@gmail.com>
2019-03-08 15:43:18 -0800

    Added files and changes required for deployment to heroku

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -14,7 +14,6 @@ import django_heroku
 import os
 import dj_database_url
 import psycopg2
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -76,7 +75,7 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = 'blog_project.wsgi.application'
-ππ
+
 # Database
 '''
 DATABASES = {
