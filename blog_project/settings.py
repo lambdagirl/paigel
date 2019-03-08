b182e9b46b74cdb853ec9ad0dcf5a47d6b0d9d8b
Peizhi Zheng <parisa1206@gmail.com>
2019-03-08 12:44:53 -0800

    upgrade django

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -135,5 +135,4 @@ REST_FTAMEWORK = {
 }
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
