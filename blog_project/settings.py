8e41ab652c61daf45960e949eddfcec6ea67239b
Peizhi Zheng <parisa1206@gmail.com>
2019-03-08 12:51:49 -0800

    upgrade django

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,5 +134,4 @@ REST_FTAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':'rest_framework.permissons.DjangoModelPermissionsOrAnonReadOnly'
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
